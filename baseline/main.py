559ca953b97f2fabb2e1785365649d65701ec4d0
Anas Shahzad <19100097@lums.edu.pk>
2023-03-24 00:00:41 +0100

    Added baseline logic

---
+++ baseline/main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import torch
+
+
+def main():
+    '''Main function starts baseline'''
+    print ("Start conducting the baseline experiment")
+
+if __name__ == '__main__':
+    main()
---
+++ datasets/entailment_converter.py
@@ -0,0 +1,14 @@
+import numpy as np
+import torch
+
+# Contains entailment pair combination for our task
+entail_combos = {}
+
+
+def main():
+    "Main function that starts the program"
+    print("Starting entailment task...")
+
+
+if __name__ == "__main__":
+    main()
